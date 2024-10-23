9b398e023c3d621784170239f7065d5b528f7ad0
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-10-23 18:08:08 +0300

    Setup router

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,8 @@
 import express from "express";
+import routes from "./routes.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("It works");
-});
+app.use(routes);
 
 app.listen(5000, () => console.log("App is listening on port http://localhost:5000"));
