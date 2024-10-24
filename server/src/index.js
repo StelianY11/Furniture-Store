94027372b2b619db3015d30e0eca798998cab7aa
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-10-25 00:50:25 +0300

    Setup DB connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import routes from "./routes.js";
+import mongoose from "mongoose";
 
 import cors from "cors";
 //import { corsMiddleware } from "./middlewares/cors.js";
+
+try {
+    await mongoose.connect("mongodb://localhost:27017", {dbName: "furniture"});
+    console.log("DB connected successfully!");
+} catch (error) {
+    console.log("Cannot connect to DB!");
+}
 
 const app = express();
 
